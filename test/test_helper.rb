a44c41d597c67bc0213b8d1daeec1dc8ef1e058d
Carsten Zimmermann <cz@aegisnet.de>
2015-07-07 20:22:07 +0200

    Fix test_helper w/ newest I18n

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,7 @@ require 'bundler/setup'
 
 require 'minitest/autorun'
 require 'active_support/test_case'
-Bundler.require(:default, :development)
-
-I18n.available_locales = :en
 
 require 'kauperts_link_checker'
+
+I18n.available_locales = :en
