792a64d721b9b77a108d01a885336763d10afa0c
Carsten Zimmermann <cz@aegisnet.de>
2015-07-07 20:33:52 +0200

    Make tests run with recent active support versions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,14 @@ require 'rubygems'
 require 'bundler/setup'
 
 
+require 'mocha'
+require 'minitest'
 require 'minitest/autorun'
+require 'active_support'
 require 'active_support/test_case'
+require 'mocha/mini_test'
 
 require 'kauperts_link_checker'
 
+ActiveSupport.test_order = :random
 I18n.available_locales = :en
