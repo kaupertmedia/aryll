785b737f919df129908aa12e1b17978cc9926d27
Carsten Zimmermann <cz@aegisnet.de>
2015-07-07 20:21:48 +0200

    WIP: Lock active_support to < 4.0 to get tests run

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,11 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require 'test/unit'
+
+require 'minitest/autorun'
 require 'active_support/test_case'
 Bundler.require(:default, :development)
+
+I18n.available_locales = :en
 
 require 'kauperts_link_checker'
