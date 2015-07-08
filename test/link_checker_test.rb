1938740b7e8436416e6ef7b2e3730a3548874fe3
Carsten Zimmermann <cz@aegisnet.de>
2015-07-08 21:38:21 +0200

    Specify network error handling

--- test/link_checker_test.rb
+++ test/link_checker_test.rb
@@ -53,7 +53,6 @@ describe Kauperts::LinkChecker do
     end
 
     describe 'with configuration options' do
-
       describe 'for trailing slashes' do
         let(:url_object) do
           Class.new { def url; 'http://www.example.com/foo' end }.new
@@ -89,7 +88,20 @@ describe Kauperts::LinkChecker do
           subject.ok?.must_equal true
         end
       end
+    end
 
+    describe 'with network errors' do
+      it 'handles timeouts' do
+        stub_net_http_error!(Timeout::Error, 'Takes way too long')
+        subject.check!
+        subject.status.must_match(/Timeout .+/)
+      end
+
+      it 'handles generic network problem' do
+        stub_net_http_error!(Class.new(StandardError), 'Somehow broken')
+        subject.check!
+        subject.status.must_match(/Generic network error .+/)
+      end
     end
 
   end
@@ -139,29 +151,6 @@ class LinkCheckerTest < ActiveSupport::TestCase
 
     assert_respond_to obj.configuration, :ignore_trailing_slash_redirects
     assert_equal true, obj.configuration.ignore_trailing_slash_redirects
-  end
-
-  test "should handle time out exceptions" do
-    stub_net_http_error!(Timeout::Error, "Takes way too long")
-    url = url_object
-    obj = checker.new(url)
-    assert_nothing_raised do
-      status = obj.check!
-      assert_kind_of String, status
-      assert_match /Timeout (.+)/, status
-    end
-  end
-
-  test "should handle generic network problem" do
-    class GenericNetworkException < Exception; end
-    stub_net_http_error!(GenericNetworkException, "Somehow broken")
-    url = url_object
-    obj = checker.new(url)
-    assert_nothing_raised do
-      status = obj.check!
-      assert_kind_of String, status
-      assert_match /Generic network error (.+)/, status
-    end
   end
 
   test "should handle domain with umlauts" do
